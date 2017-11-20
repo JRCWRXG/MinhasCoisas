82b35f8bab44b9eaee34cdc84056b640d5fd79e5
Jose Roberto <jr.campos@hotmail.com.br>
2017-11-20 16:58:56 -0200

    commit inicial...

--- Navegador_VBNET/Form3.vb
+++ Navegador_VBNET/Form3.vb
@@ -7,6 +7,8 @@ Public Class Form3
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim allements As HtmlElementCollection = WebBrowser1.Document.All
 
+        Dim teste As String
+
         For Each merda As HtmlElement In allements
 
             ListBox1.Items.Add(merda.GetAttribute("src"))
